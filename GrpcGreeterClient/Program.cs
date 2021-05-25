e980b8c7702c53ed9609f6ae4ca24dd2e15f1207
Rafael Da Silva <silvagpe@gmail.com>
2021-05-25 10:48:55 -0300

    configuração para utilizar .pfx

--- GrpcGreeterClient/Program.cs
+++ GrpcGreeterClient/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Grpc.Net.Client;
 
@@ -8,9 +9,19 @@ namespace GrpcGreeterClient
     {
         static async Task Main(string[] args)
         {
+            var httpHandler = new HttpClientHandler
+            {
+                ServerCertificateCustomValidationCallback =
+                    HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+            };
             // The port number(5001) must match the port of the gRPC server.
-            //using var channel = GrpcChannel.ForAddress("https://localhost:5001");
-            using var channel = GrpcChannel.ForAddress("https://35.225.123.164:5001");
+            //using var channel = GrpcChannel.ForAddress("https://127.0.0.1:5051");
+            
+            using var channel = GrpcChannel.ForAddress("https://127.0.0.1:5051", 
+                new GrpcChannelOptions { HttpHandler = httpHandler });
+            //using var channel = GrpcChannel.ForAddress("http://127.0.0.1:5050");
+            //using var channel = GrpcChannel.ForAddress("https://[::]:5001");
+            //using var channel = GrpcChannel.ForAddress("https://35.225.123.164:5001");
             var client =  new Greeter.GreeterClient(channel);
             var reply = await client.SayHelloAsync(
                               new HelloRequest { Name = "GreeterClient" });
